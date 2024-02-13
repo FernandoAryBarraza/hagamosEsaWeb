e9abd8287d35948d705987fb1ad60da790b3bf1d
FernandoAryBarraza <127987756+FernandoAryBarraza@users.noreply.github.com>
2024-02-13 10:52:09 -0300

    creando repo de Hagamos esa web primer proyecto

---
+++ FerchonteCVWeb/script01.js
@@ -0,0 +1,57 @@
+/* ********** Menu ********** */
+/*Todas las acciones de la funcion se hacen sobre el documento renombrado d */
+/* Funcion anonima autoejecutable */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+  /*se imtercambia el icono de 3 barras por la X y se habilita la seleccion de una opcion*/
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+  /*se imtercambia el icono de la X por 3 barras y se deshabilita la seleccion de otra opcion*/
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/* ********** ContactForm ********** */
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/your@email.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
